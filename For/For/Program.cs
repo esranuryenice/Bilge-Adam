e3eea80f4971e1f44b5a89821702290ff6e85bf5
bengisugultekin <bengisugultekin@gmail.com>
2016-11-30 13:02:50 +0300

    updated

---
+++ For/For/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Definition
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello world");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region factorial
+
+            //int n = 10;
+            //int factorial = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    factorial *= i;
+            //}
+            //Console.WriteLine("Result: " + factorial);
+            //Console.ReadKey();
+
+            #endregion
+
+            #region multipleControls
+
+            //bool status = true;
+
+            //for (int i = 0; status == true && i < 100; i++)
+            //{
+            //    if (i*2>20)
+            //    {
+            //        status = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("moving on while it is {0} ",i);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region BreakContinue
+
+            //for (int i = 0; i < 100; i = i + 5)
+            //{
+            //    if (i * 2 < 20)
+            //    {
+            //        //break; döngüyü sonlandırır
+            //        continue; //bir sonraki değere geçirir
+
+            //    }
+            //    Console.WriteLine("{0} iken devam ediyorum",i);
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region [1,n] summation
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = 0;
+
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    sum += i;
+            //    Console.WriteLine("i : {0} \t Sum : {1}", i , sum);
+            //}
+            //Console.WriteLine("Total sum: " + sum);
+            //Console.ReadKey();
+
+            #endregion
+
+            #region İcİceFor
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("\n");
+            //}
+
+            //Console.ReadKey();
+            #endregion
+
+            #region Alphabet
+
+            //for (char i = 'a'; i <= 'z'; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region MultiplicationTable
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    Console.WriteLine("{0} x {1}\t = {2}", i, j, i*j);
+                }
+                Console.WriteLine("**************");
+            }
+
+
+
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
+
+//iç içe for ile ilgili soru soracak
