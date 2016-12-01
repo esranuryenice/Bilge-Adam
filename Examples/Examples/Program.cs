c4f0636e7b60ece434560243a76681d62daaa14a
Bengisu <Bengisu@BENG>
2016-12-02 00:09:03 +0300

    upupup

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Examples
 {
@@ -12,21 +13,39 @@ namespace Examples
         {
             #region foreach
 
-            int[] numbers = { 2, 3, 5, 7, 11, 13, 17, 19 };
-            foreach (int i in numbers)
-            {
-                Console.Write(" " + i);
-            }
-            Console.WriteLine();
+            //int[] numbers = { 2, 3, 5, 7, 11, 13, 17, 19 };
+            //foreach (int i in numbers)
+            //{
+            //    Console.Write(" " + i);
+            //}
+            //Console.WriteLine();
 
-            string[] towns = { "London", "Paris", "Milan", "New York" };
-            foreach (string town in towns)
-            {
-                Console.Write(" " + town);
-            }
+            //string[] towns = { "London", "Paris", "Milan", "New York" };
+            //foreach (string town in towns)
+            //{
+            //    Console.Write(" " + town);
+            //}
 
-            Console.ReadKey();
+            //Console.ReadKey();
             #endregion
+
+            #region factorial100
+
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //BigInteger factorial = 1;
+
+            //do
+            //{
+            //    factorial *= n;
+            //    n--;
+            //} while (n > 0);
+            //Console.WriteLine("n! = " + factorial);
+            //Console.ReadKey();
+
+            #endregion
+
+
         }
     }
 }
