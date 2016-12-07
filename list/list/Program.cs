72a073b502818f963a43ded88ffcebbc227a7517
bengisugultekin <bengisugultekin@gmail.com>
2016-12-07 10:49:24 +0300

    list bitti

--- list/list/Program.cs
+++ list/list/Program.cs
@@ -12,28 +12,28 @@ namespace list
         {
             #region List
 
-            List<int> numbers = new List<int>(); //() içine bişey yazmazsan dynamic olur!
+            //List<int> numbers = new List<int>(); //() içine bişey yazmazsan dynamic olur!
 
-            numbers.Add(1);
+            //numbers.Add(1);
 
-            numbers.AddRange(new int[] { 2, 3, 4 });
+            //numbers.AddRange(new int[] { 2, 3, 4 });
 
-            Console.WriteLine(numbers.Count);
-            Console.Clear();
+            //Console.WriteLine(numbers.Count);
+            //Console.Clear();
 
-            foreach (var item in numbers)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                Console.WriteLine("{0} = {1}", i, numbers[i]);
-            }
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.WriteLine("{0} = {1}", i, numbers[i]);
+            //}
 
-            Console.ReadKey();
+            //Console.ReadKey();
 
             #endregion
 
@@ -77,11 +77,11 @@ namespace list
 
             #region List5-Silmek
 
-            List<int> numbers = new List<int>();
+            //List<int> numbers = new List<int>();
 
-            numbers.Add(3);
-            numbers.Add(5);
-            numbers.AddRange(new int[] { 8, 10, 13, 3, 8 });
+            //numbers.Add(3);
+            //numbers.Add(5);
+            //numbers.AddRange(new int[] { 8, 10, 13, 3, 8 });
 
             ////numbers.RemoveAt(0); //girilen index silinir
             ////numbers.Remove(8); //girilen değer silinir, 1den fazla ise ilk değer silinir
@@ -151,19 +151,70 @@ namespace list
 
             //var result = numbers.FindAll(i => i > 5).Count();  //5ten büyük tüm elemanların sayısını döner
 
-            numbers.Sort();
-            numbers.Reverse();
+            //numbers.Sort();
+            //numbers.Reverse();
 
-            foreach (var item in numbers)
-            {
-                Console.WriteLine("\n\t " + item);
-            }
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine("\n\t " + item);
+            //}
 
 
 
-            Console.ReadKey();
+            //Console.ReadKey();
             #endregion
 
+            #region testenAlfabe
+
+            //List<char> alphabet = new List<char>();
+
+            //for (char i = 'A'; i <= 'Z'; i++)
+            //{
+            //    alphabet.Add(i);
+               
+            //}
+
+            ////1st WAY   
+            //alphabet.Reverse();
+            //foreach (var item in alphabet)
+            //{
+            //    Console.Write(item + " ");
+            //}
+
+            ////2nd WAY
+            ////alphabet.Reverse();
+            ////for (int i = 0; i < alphabet.Count; i++)
+            ////{
+            ////    Console.Write(alphabet[i] + " ");
+            ////}
+
+            ////3rd WAY
+            ////for (int i = alphabet.Count - 1; i >= 0 ; i--)
+            ////{
+            ////    Console.Write(alphabet[i] + " ");
+            ////}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region isimler
+
+            //List<string> names = new List<string>();
+
+            //names.Add("hakan");
+            //names.AddRange(new string[] { "irfan", "aytekin", "cemal", "merve", "yankı", "atakan" });
+
+            //names.Sort();
+            //names.Reverse();
+
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine("\t" + item);
+            //}
+
+            //Console.ReadKey();
+            #endregion
 
 
 
