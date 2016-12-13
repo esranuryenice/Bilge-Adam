fd5422f131578f06e8d4b99e49bffb022a1701c3
bengisugultekin <bengisugultekin@gmail.com>
2016-12-13 13:02:39 +0300

    ekle

---
+++ bus_WinForm/bus_WinForm/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace bus_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.busType = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // busType
+            // 
+            this.busType.FormattingEnabled = true;
+            this.busType.Items.AddRange(new object[] {
+            "Travego",
+            "E403"});
+            this.busType.Location = new System.Drawing.Point(12, 40);
+            this.busType.Name = "busType";
+            this.busType.Size = new System.Drawing.Size(260, 21);
+            this.busType.TabIndex = 0;
+            this.busType.SelectedIndexChanged += new System.EventHandler(this.busType_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 447);
+            this.Controls.Add(this.busType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox busType;
+    }
+}
+
---
+++ bus_WinForm/bus_WinForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bus_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int size;
+        private void busType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string busTypeName = busType.SelectedItem.ToString();
+
+            if (busTypeName == "Travego")
+            {
+                size = 45;
+            }
+            else if (busTypeName == "E403")
+            {
+                size = 55;
+            }
+        }
+        int counter = 1;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {                   
+
+                    if (j != 2 && (i != 5 || j < 2) ) //ekle arka 5 koltuk
+                    {
+                        Button btn = new Button();
+                        btn.Width = 30;
+                        btn.Height = 30;
+                        btn.Left = btn.Width * j;
+                        btn.Top = btn.Width * i;
+                        btn.Text = counter.ToString();
+                        this.Controls.Add(btn);
+                        counter++;
+                    }
+
+                    //else if ((i != 5 && j != 3) || (i != 5 && j != 4))
+                    //{
+                    //    Button btn = new Button();
+                    //    btn.Width = 30;
+                    //    btn.Height = 30;
+                    //    btn.Left = btn.Width * j;
+                    //    btn.Top = btn.Width * i;
+                    //    btn.Text = counter.ToString();
+                    //    this.Controls.Add(btn);
+                    //    counter++;
+                        
+                    //}
+
+                }
+
+            }
+        }
+    }
+
+}
+
+//this.Controls.Clear();
+//ototbüs tipine göre buton oluştur
