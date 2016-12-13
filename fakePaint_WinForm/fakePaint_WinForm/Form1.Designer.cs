db4c9f814ac1dcb2561541c4cc0235375255aa1b
bengisugultekin <bengisugultekin@gmail.com>
2016-12-13 11:28:36 +0300

    fake paint bitti

---
+++ fakePaint_WinForm/fakePaint_WinForm/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace fakePaint_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseWidth = new System.Windows.Forms.ComboBox();
+            this.buttonChooseColor = new System.Windows.Forms.Button();
+            this.buttonErase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chooseWidth
+            // 
+            this.chooseWidth.FormattingEnabled = true;
+            this.chooseWidth.Items.AddRange(new object[] {
+            "1",
+            "3",
+            "5",
+            "8",
+            "11",
+            "14",
+            "20",
+            "25"});
+            this.chooseWidth.Location = new System.Drawing.Point(12, 12);
+            this.chooseWidth.Name = "chooseWidth";
+            this.chooseWidth.Size = new System.Drawing.Size(121, 21);
+            this.chooseWidth.TabIndex = 0;
+            this.chooseWidth.SelectedIndexChanged += new System.EventHandler(this.chooseWidth_SelectedIndexChanged);
+            // 
+            // buttonChooseColor
+            // 
+            this.buttonChooseColor.Location = new System.Drawing.Point(150, 12);
+            this.buttonChooseColor.Name = "buttonChooseColor";
+            this.buttonChooseColor.Size = new System.Drawing.Size(75, 23);
+            this.buttonChooseColor.TabIndex = 1;
+            this.buttonChooseColor.Text = "Color";
+            this.buttonChooseColor.UseVisualStyleBackColor = true;
+            this.buttonChooseColor.Click += new System.EventHandler(this.buttonChooseColor_Click);
+            // 
+            // buttonErase
+            // 
+            this.buttonErase.Location = new System.Drawing.Point(240, 12);
+            this.buttonErase.Name = "buttonErase";
+            this.buttonErase.Size = new System.Drawing.Size(75, 23);
+            this.buttonErase.TabIndex = 2;
+            this.buttonErase.Text = "Erase";
+            this.buttonErase.UseVisualStyleBackColor = true;
+            this.buttonErase.Click += new System.EventHandler(this.buttonErase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 261);
+            this.Controls.Add(this.buttonErase);
+            this.Controls.Add(this.buttonChooseColor);
+            this.Controls.Add(this.chooseWidth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox chooseWidth;
+        private System.Windows.Forms.Button buttonChooseColor;
+        private System.Windows.Forms.Button buttonErase;
+    }
+}
+
---
+++ fakePaint_WinForm/fakePaint_WinForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fakePaint_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        ColorDialog colorDialog = new ColorDialog();
+        int startX, startY;
+        int width = 2;
+        bool draw;
+
+       
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+            Pen p = new Pen(colorDialog.Color, width); //color ve width ekle
+
+            Point point1 = new Point(startX, startY);
+            Point point2 = new Point(e.X, e.Y);
+
+            if (draw == true)
+            {
+                g.DrawLine(p, point1, point2);
+                startX = e.X;
+                startY = e.Y;
+                               
+            }
+
+        }
+
+        private void chooseWidth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            width = int.Parse(chooseWidth.SelectedItem.ToString());
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            draw = true;
+            startX = e.X;
+            startY = e.Y;
+        }
+
+        private void buttonErase_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void buttonChooseColor_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+        }
+
+
+    }
+}
